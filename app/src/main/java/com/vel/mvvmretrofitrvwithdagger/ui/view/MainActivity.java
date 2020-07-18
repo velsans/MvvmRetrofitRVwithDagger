6cca85a06eac4aaeea93702454dc702dd8c901aa
Velmurugan <velsans@gmail.com>
2020-07-19 00:00:25 +0530

    created project structure

--- app/src/main/java/com/vel/mvvmretrofitrvwithdagger/ui/view/MainActivity.java
+++ app/src/main/java/com/vel/mvvmretrofitrvwithdagger/ui/view/MainActivity.java
@@ -1,9 +1,9 @@
-package com.vel.mvvmretrofitrvwithdagger;
+package com.vel.mvvmretrofitrvwithdagger.ui.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.vel.mvvmretrofitrvwithdagger.ui.main.MainFragment;
+import com.vel.mvvmretrofitrvwithdagger.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/vel/mvvmretrofitrvwithdagger/ui/view/MainFragment.java
+++ app/src/main/java/com/vel/mvvmretrofitrvwithdagger/ui/view/MainFragment.java
@@ -1,4 +1,4 @@
-package com.vel.mvvmretrofitrvwithdagger.ui.main;
+package com.vel.mvvmretrofitrvwithdagger.ui.view;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import  com.vel.mvvmretrofitrvwithdagger.ui.main.MainViewModel;
 
 import com.vel.mvvmretrofitrvwithdagger.R;
 
